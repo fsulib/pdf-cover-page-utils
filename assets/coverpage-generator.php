89786d1bf0ab8f28f8c08d6e4017001592a7c311
Aaron Retteen <ajr12@my.fsu.edu>
2016-12-20 15:09:34 -0500

    Aaron switches FPDF to tFPDF library to add unicode support

--- assets/coverpage-generator.php
+++ assets/coverpage-generator.php
@@ -1,8 +1,40 @@
 #!/usr/bin/env php
 <?php
 
+require_once(__DIR__ . '/tfpdf/tfpdf.php');
 
-require_once(__DIR__ . '/fpdf181/fpdf.php');
+// map FPDF to tFPDF so FPDF_TPL can extend it
+class FPDF extends tFPDF
+{
+    /**
+     * "Remembers" the template id of the imported page
+     */
+    protected $_tplIdx;
+
+    /**
+     * Draw an imported PDF logo on every page
+     */
+    public function Header()
+    {
+        if (is_null($this->_tplIdx)) {
+            $this->setSourceFile(__DIR__ . '/coverpage.pdf');
+            $this->_tplIdx = $this->importPage(1);
+        }
+        /* The rest of this function was originally included but is not useful for our purposes
+        
+        $size = $this->useTemplate($this->_tplIdx, 130, 5, 60);
+
+        $this->SetFont('Times', '', 16);
+        $this->SetTextColor(0);
+        $this->SetXY($this->lMargin, 5);
+
+        $text = 'tFPDF (v' . tFPDF_VERSION . ') and FPDI (v'
+            . FPDI::VERSION . ')';
+        $this->Cell(0, $size['h'], $text);
+        $this->Ln(); */
+    }
+}
+
 require_once(__DIR__ . '/FPDI-1.6.1/fpdi.php');
 
 ini_set('display_errors',1);  
@@ -46,7 +78,7 @@ else {
 
 // Get publication date
 $pubDate = $mods->originInfo->dateIssued;
-
+$pubnote = "";
 // Get publication note
 foreach ($mods->note as $note) {
   if ($note['displayLabel'] == "Publication Note") {
@@ -57,11 +89,16 @@ foreach ($mods->note as $note) {
 // Add coverpage
 $pdf = new FPDI();
 $pdf->AddPage('P', 'Letter');
-$pdf->setSourceFile(__DIR__. "/coverpage.pdf");
-$tplIdx = $pdf->importPage(1);
-$pdf->useTemplate($tplIdx);
-$pdf->SetTextColor(0, 0, 0);
-$pdf->SetFont('Times');
+//$pdf->setSourceFile(__DIR__. "/coverpage.pdf");
+ $tplIdx = $pdf->importPage(1);
+ $pdf->useTemplate($tplIdx);
+ $pdf->SetTextColor(0, 0, 0);
+// $pdf->SetFont('Times');
+
+
+// Add some Unicode font (uses UTF-8)
+$pdf->AddFont('DejaVuSerif', '', 'DejaVuSerif.ttf', true);
+$pdf->SetFont('DejaVuSerif', '');
 
 $pdf->setFontSize(14);
 $pdf->SetXY(25, 55);
@@ -76,10 +113,12 @@ $pdf->setLeftMargin(25);
 $pdf->SetY($pdf->GetY() + 3);
 $pdf->MultiCell(0, 5, $preppedAuthors, 0, 'L');
 
+if($pubnote){
 $pdf->setFontSize(8);
 $pdf->SetY($pdf->GetY() + 5);
 $pdf->MultiCell(175, 5, $pubNote, 0, 'L');
+}
 
-$pdf->Output('F', $argv[2]);
+$pdf->Output($argv[2], 'F'); // tFPDF switches the argument parameters for Output();
 
 ?>
